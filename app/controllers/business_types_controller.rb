2a922a51d8f9e76cf44d4c4ec97a4afe2277cd18
Cody Brubaker and Tyler Ferrin <cody@>
2017-02-06 10:07:33 -0800

    finish CRUD functionality

--- app/controllers/business_types_controller.rb
+++ app/controllers/business_types_controller.rb
@@ -4,6 +4,46 @@ class BusinessTypesController < ApplicationController
   end
 
   def show
-    @business_types = BusinessType.find(params[:id])
+    @business_type = BusinessType.find(params[:id])
   end
+
+  def new
+    @business_type = BusinessType.new
+  end
+
+  def create
+    @business_type = BusinessType.new(business_type_params)
+    if @business_type.save
+      redirect_to business_types_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @business_type = BusinessType.find(params[:id])
+  end
+
+  def update
+    @business_type = BusinessType.find(params[:id])
+    if @business_type.update(business_type_params)
+      redirect_to business_types_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @business_type = BusinessType.find(params[:id])
+    @business_type.destroy
+    redirect_to business_types_path
+  end
+
+private
+
+  def business_type_params
+    params.require(:business_type).permit(:name)
+  end
+
+
 end
